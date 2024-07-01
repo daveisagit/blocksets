984ab48e29e504ad4abc0a4b6765fd2b0484eb50
Dave Budd <david.budd@ovo.com>
2024-07-01 17:00:20 +0100

    Initial commit

---
+++ tests/block_data.py
@@ -0,0 +1,99 @@
+"""
+Block arrangements for tests and fixtures
+"""
+
+from blocksets.classes.block import Block
+from util import generate_test_set_1D, multiple_of_tuple
+
+
+def blocks_1D_arbitrary_set_1():
+    """
+    ----
+            ----
+                    ----
+        ----
+          ----
+              --------
+      --------------
+    """
+    blocks = []
+    blocks.append(Block(0, 4))
+    blocks.append(Block(8, 12))
+    blocks.append(Block(16, 20))
+    blocks.append(Block(4, 8))
+    blocks.append(Block(6, 10))
+    blocks.append(Block(10, 18))
+    blocks.append(Block(2, 16))
+    return blocks
+
+
+def blocks_1D_all_arrangements_of_2(scale=5, points=[4, 7, 12, 16]):
+    arrangements = []
+    for test_set in generate_test_set_1D(2):
+        if points:
+            arrangements.append([Block(points[a], points[b]) for a, b in test_set])
+            continue
+        arrangements.append([Block(a * scale, b * scale) for a, b in test_set])
+    return arrangements
+
+
+def blocks_1D_all_arrangements_of_3(scale=5):
+    arrangements = []
+    for test_set in generate_test_set_1D(3):
+        arrangements.append([Block(a * scale, b * scale) for a, b in test_set])
+    return arrangements
+
+
+def blocks_2D_arbitrary_set_example():
+    """As per the example in the README"""
+    blocks = []
+    blocks.append(Block((-8, 11), (-3, 15)))
+    blocks.append(Block((-5, 8), (1, 16)))
+    blocks.append(Block((-1, 2), (6, 18)))
+    blocks.append(Block((-9, 6), (8, 9)))
+    return blocks
+
+
+def blocks_2D_all_arrangements_of_2(scale=5):
+    arrangements = []
+    interval_pairs = generate_test_set_1D(2)
+    for x_pair in interval_pairs:
+        for y_pair in interval_pairs:
+
+            a = (x_pair[0][0], y_pair[0][0])
+            b = (x_pair[0][1], y_pair[0][1])
+            a = multiple_of_tuple(a, scale)
+            b = multiple_of_tuple(b, scale)
+            r1 = Block(a, b)
+
+            a = (x_pair[1][0], y_pair[1][0])
+            b = (x_pair[1][1], y_pair[1][1])
+            a = multiple_of_tuple(a, scale)
+            b = multiple_of_tuple(b, scale)
+            r2 = Block(a, b)
+            arrangements.append([r1, r2])
+
+    return arrangements
+
+
+def blocks_3D_all_arrangements_of_2(scale=5):
+    arrangements = []
+    interval_pairs = generate_test_set_1D(2)
+    for x_pair in interval_pairs:
+        for y_pair in interval_pairs:
+            for z_pair in interval_pairs:
+
+                a = (x_pair[0][0], y_pair[0][0], z_pair[0][0])
+                b = (x_pair[0][1], y_pair[0][1], z_pair[0][1])
+                a = multiple_of_tuple(a, scale)
+                b = multiple_of_tuple(b, scale)
+                r1 = Block(a, b)
+
+                a = (x_pair[1][0], y_pair[1][0], z_pair[1][0])
+                b = (x_pair[1][1], y_pair[1][1], z_pair[1][1])
+                a = multiple_of_tuple(a, scale)
+                b = multiple_of_tuple(b, scale)
+                r2 = Block(a, b)
+                arrangements.append([r1, r2])
+
+    return arrangements
---
+++ tests/util.py
@@ -0,0 +1,105 @@
+"""Code to assist testing"""
+
+from collections import deque
+from itertools import combinations
+from blocksets.classes.block import Block
+from blocksets.classes.blockset import BlockSet, OperationType
+
+
+def apply_to_tuple_set(s: set, op_type: OperationType, block: Block):
+    """We trust the manipulation of tuple points in a set
+    This will provide a means to verify results from normalisation when tests
+    cases are generated dynamically
+
+    Args:
+        s (set): A set
+        op_type (OperationType): Add, Remove or Toggle
+        block (Block): A block object
+    """
+    for t in block.lattice():
+        if op_type == OperationType.ADD:
+            s.add(t)
+        if op_type == OperationType.REMOVE:
+            s.discard(t)
+        if op_type == OperationType.TOGGLE:
+            if t in s:
+                s.discard(t)
+            else:
+                s.add(t)
+
+
+def apply_to_block_set(block_set: BlockSet, op_type: OperationType, block: Block):
+    """Simple helper for testing
+
+    Args:
+        block_set (BlockSet): A BlockSet object
+        op_type (OperationType): Add, Remove or Toggle
+        block (Block): A block object
+    """
+    if op_type == OperationType.ADD:
+        block_set.add(block)
+    if op_type == OperationType.REMOVE:
+        block_set.remove(block)
+    if op_type == OperationType.TOGGLE:
+        block_set.toggle(block)
+
+
+def block_set_to_tuple_set(bs: BlockSet) -> set:
+    """Covert a BlockSet to a set of tuple points
+
+    Args:
+        bs (BlockSet): A given BlockSet object
+
+    Returns:
+        set: The equivalent as a set of tuple points
+    """
+    tuple_set = set()
+    for b in bs.blocks():
+        tuple_set.update(b.lattice())
+    return tuple_set
+
+
+def generate_test_set_1D(n: int) -> set:
+    """Generates all possible arrangements of intervals on a line
+    for a given number of intervals required.
+
+    Args:
+        n (int): The number of intervals required in a test set
+
+    Returns:
+        set: A set of tuples, each tuple being a set of intervals
+    """
+    test_set = set()
+
+    # perform a BFS for all ways to construct n intervals there are multiple
+    # roots for the search from 2 points up to 2n points
+    # if we don't use all the points then reject the outcome
+
+    bfs = deque()
+    for points in range(2, 2 * n + 1):
+        point_usage = [0] * points
+        state = (tuple(point_usage), tuple())
+        bfs.append(state)
+
+    while bfs:
+        point_usage, blocks = bfs.popleft()
+        points = len(point_usage)
+        if len(blocks) == n:
+            if all(True if pu > 0 else False for pu in point_usage):
+                test_set.add(blocks)
+            continue
+        for a, b in combinations(range(points), 2):
+            a, b = min(a, b), max(a, b)
+            new_point_usage = list(point_usage)
+            new_point_usage[a] += 1
+            new_point_usage[b] += 1
+            new_blocks = list(blocks)
+            new_blocks.append((a, b))
+            new_state = tuple(new_point_usage), tuple(new_blocks)
+            bfs.append(new_state)
+
+    return test_set
+
+
+def multiple_of_tuple(t, m):
+    return tuple([m * x for x in t])
